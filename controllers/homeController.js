8b956d228daf44c3e107fe3d024eb3a44eebde6d
FriedTurnips <>
2014-09-10 14:43:04 -0400

    Initial project commit
    Version: 0.1.0
    This release outputs simple html to the browser window using a
    controller

---
+++ controllers/homeController.js
@@ -0,0 +1,11 @@
+﻿(function (homeController) {
+
+    homeController.init = function(app) {
+        app.get("/", function (req, res) {
+            res.render("index", { title: "Express + Vash" });
+        });
+    };
+})
+
+(module.exports);
+  
---
+++ server.js
@@ -0,0 +1,19 @@
+﻿var http = require('http');
+var express = require('express')
+var app = express();
+var port = process.env.port || 1337;
+var controllers = require("./controllers");
+
+//Set Up the view engine
+app.set("view engine", "vash");
+
+//Map the routes
+controllers.init(app);
+
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send({ name: "Neil", isValid: true, group: "Admin" });
+});
+
+var server = http.createServer(app)
+server.listen(port);
